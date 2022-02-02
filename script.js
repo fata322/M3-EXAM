3d5d8595ed62d24be00d9fc20b55ba7c33491592
Marc Pacaldo <fatafatafata322@gmail.com>
2022-02-03 07:58:59 +0800

    added navbar and edited some styles

--- script.js
+++ script.js
@@ -33,10 +33,40 @@ function getMealList() {
         });
         mealList.classList.remove("notFound");
       } else {
-        html = "Sorry, we didn't find any meal!";
+        html = "Sorry, we didn't find any recipes!";
         mealList.classList.add("notFound");
       }
 
       mealList.innerHTML = html;
     });
+}
+
+function getMealRecipe(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("recipe-btn")) {
+    let mealItem = e.target.parentElement.parentElement;
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
+    )
+      .then((response) => response.json())
+      .then((data) => mealRecipeModal(data.meals));
+  }
+}
+
+function mealRecipeModal(meal) {
+  console.log(meal);
+  meal = meal[0];
+  let html = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+    `;
+  mealDetailsContent.innerHTML = html;
+  mealDetailsContent.parentElement.classList.add("showRecipe");
 }
